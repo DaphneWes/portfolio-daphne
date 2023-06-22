84583dd19eb1b46b829256a965e6069dc346cb9c
Daphne <daphne.westerdijk@hotmail.com>
2023-06-22 16:51:00 +0200

    changed images path

--- src/components/Portfolio/PortfolioInfo.js
+++ src/components/Portfolio/PortfolioInfo.js
@@ -4,21 +4,21 @@ const portfolioInfo = () => {
             id: 0,
             name: "Travel App",
             info: "Lorem Ipsum",
-            img: "portfolio-daphne/images/TravelApp.png",
+            img: "/portfolio-daphne/images/TravelApp.png",
             link: "https://daphnewes.github.io/Travel/"
         },
         {
             id: 1,
             name: "Weather App",
             info: "Lorem Ipsum",
-            img: "portfolio-daphne/images/WeatherApp.png",
+            img: "/portfolio-daphne/images/WeatherApp.png",
             link: "https://daphnewes.github.io/react-weather-app/"
         },
         {
             id: 2,
             name: "Movie Library App",
             info: "Lorem Ipsum",
-            img: "portfolio-daphne/images/MovieLibraryApp.png",
+            img: "/portfolio-daphne/images/MovieLibraryApp.png",
             link: "https://daphnewes.github.io/movie-library/"
         }
     ]
