3d5e0d91588801a8bcfe86ea018ac8f55082d1ba
Daphne <daphne.westerdijk@hotmail.com>
2023-06-26 15:23:22 +0200

    added popquiz card and hover video functionality

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,27 +1,48 @@
 import "./Portfolio.css"
 import portfolioInfo from "./PortfolioInfo";
 
-function Portfolio (props) {
+function Portfolio(props) {
     const projects = portfolioInfo();
 
-    return(
+    const playMovie = (e) => {
+        e.target.play();
+    }
+
+    const stopMovie = (e) => {
+        e.target.pause();
+    }
+
+    return (
         <div id="portfolio" className={`container-portfolio ${props.theme}`}>
             <div className="row">
                 <div className="num-portfolio">02</div>
                 <div className="title-portfolio">
                     Projects
                 </div>
-                <hr/>
+                <hr />
             </div>
             <div className="projects-grid">
                 {projects.map((project) => (
                     <li key={project.id}>
-                        <img src={project.img} alt="screenshot-webpage"></img>
+                        {!project.img ? <></>
+                            :
+                            <img src={project.img} alt="screenshot-webpage"></img>
+                        }
+                        {!project.video ? <></>
+                            :
+                            <div className="video-portfolio">
+                                <div className="hover-play">Hover to play</div>
+                                <video src={project.video} alt="screenshot-webpage" onMouseOver={playMovie} onMouseOut={stopMovie} loop muted></video>
+                            </div>
+                        }
                         <div className="project-card">
                             <h2>{project.name}</h2>
                             <p>{project.info}</p>
                             <form>
-                                <a href={project.link} target="_blank" rel="noreferrer">Link</a>
+                                {!project.link ? <></>
+                                    :
+                                    <a href={project.link} target="_blank" rel="noreferrer">Link</a>
+                                }
                             </form>
                         </div>
                     </li>
--- src/components/Portfolio/PortfolioInfo.js
+++ src/components/Portfolio/PortfolioInfo.js
@@ -3,23 +3,30 @@ const portfolioInfo = () => {
         {
             id: 0,
             name: "Travel App",
-            info: "Lorem Ipsum",
+            info: "A frontend react app for a travel website.",
             img: "/portfolio-daphne/images/TravelApp.png",
             link: "https://daphnewes.github.io/Travel/"
         },
         {
             id: 1,
             name: "Weather App",
-            info: "Lorem Ipsum",
+            info: "A Weather App using the Open Weather Map and GeoDB Cities APIs.",
             img: "/portfolio-daphne/images/WeatherApp.png",
             link: "https://daphnewes.github.io/react-weather-app/"
         },
         {
             id: 2,
             name: "Movie Library App",
-            info: "Lorem Ipsum",
+            info: "An IMDB parody app using the IMDb rapid API.",
             img: "/portfolio-daphne/images/MovieLibraryApp.png",
             link: "https://daphnewes.github.io/movie-library/"
+        }
+        ,
+        {
+            id: 3,
+            name: "Popquiz App",
+            info: "A Popquiz app using the spotify API. Hover over the video and it will play. The app is still in spotify development mode and can therefore not be published and used in github pages ",
+            video: "/portfolio-daphne/images/Popquiz-app.mp4"
         }
     ]
     
